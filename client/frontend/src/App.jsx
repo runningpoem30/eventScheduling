570eca827cc00884c8e9f1fb2015f57e90019e01
ArchieTansaria <archietans.work@gmail.com>
2024-12-12 18:17:25 +0530

    first

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -1,29 +1,25 @@
-import { useState } from 'react'
-import SignUp from './components/SignUp/SignUp'
-import Login from './components/Login/Login'
-import './App.css'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
-import FirstNavbar from './components/firstNavbar/FirstNavbar'
-
+import { useState } from "react";
+import SignUp from "./components/SignUp/SignUp";
+import Login from "./components/Login/Login";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FirstNavbar from "./components/firstNavbar/FirstNavbar";
 
 function App() {
-
-
-  return (
-    <>
-      <div>
-        <BrowserRouter>
-        <Routes>
-          <Route path='/home' element={<FirstNavbar/>}></Route>
-          <Route path='/signup' element={<SignUp/>}/>
-          <Route path='/login' element={<Login/>}/>
-        </Routes>
-        </BrowserRouter>
-
-      </div>
-      
-    </>
-  )
+	return (
+		<>
+			<div>
+				<BrowserRouter>
+					<FirstNavbar />
+					<Routes>
+						{/* <Route path="/home" element={<FirstNavbar />} /> */}
+						<Route path="/signup" element={<SignUp />} />
+						<Route path="/login" element={<Login />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
--- client/frontend/src/components/Login/Login.jsx
+++ client/frontend/src/components/Login/Login.jsx
@@ -1,57 +1,59 @@
-import { useState } from 'react'
-import styles from './Login.module.css'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import { useState } from "react";
+import styles from "./Login.module.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+	const [email, setEmail] = useState();
+	const [password, setPassword] = useState();
+	const navigate = useNavigate();
 
-  const[email , setEmail] = useState();
-  const [password , setPassword] = useState();
-  const navigate = useNavigate()
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		axios
+			.post("http://localhost:5001/user/login", { email, password })
+			.then((result) => {
+				console.log(result);
+				if (result.data === "success") {
+					navigate("/home");
+				}
+			});
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    axios.post("http://localhost:5001/user/login" , ({email , password}))
-    .then((result) => {
-      console.log(result)
-      if(result.data === "success"){
-        navigate('/home')
-      }
-    })
-  }
+	return (
+		<div className={styles.container}>
+			<div className="text-[40px] font-bold">Log in </div>
+			<div className={styles.content}>
+				<form onSubmit={handleSubmit}>
+					<div className={styles.user_details}>
+						<div className={styles.input_box}>
+							<span className={styles.details}>Email</span>
+							<input
+								type="text"
+								className="h-[45px] w-[512px] text-base border border-black pl-4 rounded-md"
+								onChange={(e) => setEmail(e.target.value)}
+							></input>
+						</div>
 
-  return (
-  
-    <div className={styles.container}>
-    <div className="text-[40px] font-bold">Log in </div>
-    <div className={styles.content}>
+						<div className={styles.input_box}>
+							<span className={styles.details}>Password</span>
+							<input
+								type="text"
+								className="h-[45px] w-[512px] text-base border border-black pl-4 rounded-md"
+								onChange={(e) => setPassword(e.target.value)}
+							></input>
+						</div>
 
-      <form onSubmit={handleSubmit}>
-        <div className={styles.user_details}>
+						<div className="button">
+							<button className="ml-[200px] mt-[10px] bg-[#3793FF] px-10 text-[white] py-3 rounded-[5px]">
+								Submit
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
 
-          <div className={styles.input_box}>
-            <span className={styles.details}>Email</span>
-            <input type="text" className="h-[45px] w-[512px] text-base border border-black pl-4 rounded-md"
-            onChange={(e) => setEmail(e.target.value)}></input>
-          </div>
-
-          <div className={styles.input_box}>
-            <span className={styles.details}>Password</span>
-            <input type="text" className="h-[45px] w-[512px] text-base border border-black pl-4 rounded-md"
-            onChange={(e) => setPassword(e.target.value)}></input>
-          </div>
-
-          <div className="button">
-          <button className='ml-[200px] mt-[10px] bg-[#3793FF] px-10 text-[white] py-3 rounded-[5px]' >Submit</button>
-        </div>
-        </div>
-       </form>
-  </div>
-   </div>
-  )
-}
-
-
-export default Login
-
-
+export default Login;
