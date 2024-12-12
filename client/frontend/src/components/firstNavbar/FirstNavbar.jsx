1ee1ec98ee52b16aa3e9c04ff52011d110e22bc9
ArchieTansaria <archietans.work@gmail.com>
2024-12-12 18:29:58 +0530

    second

--- client/frontend/src/components/firstNavbar/FirstNavbar.jsx
+++ client/frontend/src/components/firstNavbar/FirstNavbar.jsx
@@ -1,19 +1,34 @@
-import slotifyLogo from "/Users/aryapathak/eventSchedulingApp/client/frontend/src/assets/images/slotifyLogo.png";
+import slotifyLogo from "../../assets/images/slotifyLogo.png";
 
 const FirstNavbar = () => {
 	return (
-		<div className="absolute top-0 left-0 w-full mt-50px">
-			<nav className="flex">
-				<img src={slotifyLogo} className="h-[25px]"></img>
-				<ul className="flex gap-[40px] ml-[100px] ">
-					<li>Product</li>
-					<li>Solutions</li>
-					<li>Enterprise</li>
-					<li>Pricing</li>
-					<li>Resources</li>
-					<li>Signup</li>
+		<div className="absolute top-0 left-0 w-full mt-[50px] border-b-2 border-b-black-100 ">
+			<nav className="flex mb-[5px]  ">
+				<img
+					src={slotifyLogo}
+					className="h-[45px] mb-[20px] ml-[25px]"
+				></img>
+				<ul className="flex gap-[50px] ml-[400px] ">
+					<li className="text-[18px] text-[#2C2C2C] font-bold">
+						Product
+					</li>
+					<li className="text-[18px] text-[#2C2C2C] font-bold">
+						Solutions
+					</li>
+					<li className="text-[18px] text-[#2C2C2C] font-bold">
+						Enterprise
+					</li>
+					<li className="text-[18px] text-[#2C2C2C] font-bold">
+						Pricing
+					</li>
+					<li className="text-[18px] text-[#2C2C2C] font-bold">
+						Resources
+					</li>
+					<li className="text-[15px] text-[#2c2c2c] font-normal mt-[3px]">
+						Signup
+					</li>
 				</ul>
-				<button className="bg-[#3793FF] px-3 text-[white]  rounded-[5px]">
+				<button className="bg-[#3793FF] py-[3px] px-[8px] text-[white]  rounded-[8px] ml-[20px] align-center mb-[30px]">
 					Get Started
 				</button>
 			</nav>
